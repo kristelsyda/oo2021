4679cf011e89261124c9dddc2493825f86e43c79
Kristel Süda <kristelsyda@users.noreply.github.com>
2021-02-12 13:08:07 +0200

    Parandus

--- Kodutood/kodutoo1.java
+++ Kodutood/kodutoo1.java
@@ -2,26 +2,32 @@ import java.util.Scanner;
 
 public class kodutoo1{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Sisesta esimene number: ");
-        double a = in.nextDouble();
-        System.out.println("Sisesta teine number: ");
-        double b = in.nextDouble();
-        System.out.println("Sisesta kolmas number: ");
-        double c = in.nextDouble();
-
-        if(a >= b){
-            if(a >= c)
-                System.out.println(a+" on suurim number");
-            else
-                System.out.println(c+" on suurim number");
+        Scanner number = new Scanner(System.in);
+        try {
+            System.out.println("Sisesta esimene number: ");
+            double a = number.nextDouble();
+            System.out.println("Sisesta teine number: ");
+            double b = number.nextDouble();
+            System.out.println("Sisesta kolmas number: ");
+            double c = number.nextDouble();
+            
+            if(a >= b){
+                if(a >= c)
+                    System.out.println(a+" on suurim number");
+                else
+                    System.out.println(c+" on suurim number");
+            }
+            else{
+                if(b >= c)
+                    System.out.println(b+" on suurim  number");
+                else
+                    System.out.println(c+" on suurim number"); 
+            }
+        } finally {
+            number.close();
         }
-        else{
-            if(b >= c)
-                System.out.println(b+" on suurim  number");
-            else
-                System.out.println(c+" on suurim number"); 
-        }
-        
     }
+    
+        
 }
+
